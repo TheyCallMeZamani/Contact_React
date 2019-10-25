b416875159dbd06ed6c42a2f55d7492d565736bf
theycallmezamani <microcosm22000@yahoo.com>
2019-10-25 15:56:38 +0330

    Created A Function Component !

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Xamani</h4>
+        <ul>
+          <li>Email: JohnDoe@gmail.com</li>
+          <li>Phone: 555-555-333</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
