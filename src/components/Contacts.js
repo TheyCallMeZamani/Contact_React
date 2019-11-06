e28dfb4a2ed6bc0b5964be5ce486ed60c1fe86f0
theycallmezamani <microcosm22000@yahoo.com>
2019-11-06 23:23:52 +0330

    Create Reducer !!

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,38 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "Mahdi Xamani",
-        email: "Mahdi@zamani.ir",
-        phone: "913-410-7239"
-      },
-      {
-        id: 2,
-        name: "Lale Eskandari",
-        email: "setare@zamani.ir",
-        phone: "913-333-2222"
-      },
-      {
-        id: 3,
-        name: "Pooneh Eskandari",
-        email: "lale@zamani.ir",
-        phone: "912-410-1111"
-      }
-    ]
-  };
-
   render() {
-    const { contacts } = this.state;
     return (
-      <div>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </div>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
---
+++ src/context.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+const Context = React.createContext();
+// Reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Mahdi Xamani",
+        email: "Mahdi@zamani.ir",
+        phone: "913-410-7279"
+      },
+      {
+        id: 2,
+        name: "Lale Eskandari",
+        email: "setare@zamani.ir",
+        phone: "913-333-2222"
+      },
+      {
+        id: 3,
+        name: "Pooneh Eskandari",
+        email: "lale@zamani.ir",
+        phone: "912-410-1111"
+      }
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
+  };
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
